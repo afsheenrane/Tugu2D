e10a85b340694c52bd14dc0b79dbe6348796c104
Afsheen Rane <afsheen_rane@hotmail.com>
2015-06-24 20:52:11 -0400

    Made some progress I think.

--- src/phys2d/collisionLogic/collisionManagers/SpeculativeManager2.java
+++ src/phys2d/collisionLogic/collisionManagers/SpeculativeManager2.java
@@ -4,42 +4,57 @@
 package phys2d.collisionLogic.collisionManagers;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
+import phys2d.Phys2DMain;
+import phys2d.collisionLogic.spacePartitioning.BSPTree;
+import phys2d.collisionLogic.spacePartitioning.SweptBSPTree;
+import phys2d.entities.Vec2D;
 import phys2d.entities.shapes.Shape;
+import phys2d.entities.shapes.polygons.Rectangle;
 
 /**
- * This class manages all collisions and collision resolutions that take
- * place within the simulation. It uses the 2nd version of the GJK-EPA
- * collision checker and (hopefully) implements speculative contacts.
+ * This class manages all collisions and collision resolutions that take place
+ * within the simulation. It uses the 2nd version of the GJK-EPA collision
+ * checker and (hopefully) implements speculative contacts.
  * 
  * @author afsheen
  */
 public class SpeculativeManager2 extends CollisionManager {
 
+    private SweptBSPTree collisionTree;
+
     /**
-     * @param dt
+     * Create a new manager which uses GJKv2 and speculative contacts.
+     * 
+     * @param dt the timestep of this simulation.
      */
     public SpeculativeManager2(double dt) {
         super(dt);
-        // TODO Auto-generated constructor stub
+        collisionTree = new SweptBSPTree(new Rectangle(new Vec2D(500, 500),
+                Phys2DMain.XRES + 50, Phys2DMain.YRES + 50),
+                BSPTree.HORIZONTAL_SPLIT, 1, dt);
     }
 
     @Override
-    protected void manageCollisions(Shape[] entities) {
-        // TODO Auto-generated method stub
+    protected void manageCollisions(ArrayList<Shape> entities) {
+        collisionTree.refresh();
+        HashSet<CollisionPairs> collidedPairs = new HashSet<CollisionPairs>(
+                entities.size());
+
+        for (Shape s : entities) {
+            collisionTree.insert(s);
+        }
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * phys2d.collisionLogic.collisionManagers.CollisionManager#runManager(java
-     * .util.ArrayList)
-     */
     @Override
     public void runManager(ArrayList<Shape> entities) {
-        // TODO Auto-generated method stub
+        // addWorldForces(entities);
+
+        manageCollisions(entities);
+
+        moveEntities(entities);
 
     }
 
