9ec7c9fad1ec8826b110d0128013a248a70bf98e
afsheen <afsheen_rane@hotmail.com>
2015-06-27 20:07:23 -0400

    Returned Vec2D.ORIGIN as opposed to a brand new Vec2D(0,0) object.

--- src/phys2d/entities/shapes/polygons/WorldBound.java
+++ src/phys2d/entities/shapes/polygons/WorldBound.java
@@ -4,33 +4,33 @@ import phys2d.entities.Material;
 import phys2d.entities.Vec2D;
 
 public final class WorldBound extends Rectangle {
-	
-	public WorldBound(Vec2D center, double length, double height, double angle) {
-		super(center, length, height, angle, Material.REFLECTIUM);
-		setMass(0);
-	}
 
-	public WorldBound(Vec2D center, double length, double height) {
-		this(center, length, height, 0);
-	}	
-	
-	@Override
-	public Vec2D getVelocity(){
-		return new Vec2D();
-	}
-	
-	@Override
-	public void translate(Vec2D trans){
-		//Do Nothing.
-	}
-	
-	@Override
-	public void move(double dt){
-		//Do nothing. Because world bounds are immovable.
-	}
-	
-	@Override
-	public void setVelocity(Vec2D velocity){
-		//Do nothing. World bounds are immovable.
-	}	
+    public WorldBound(Vec2D center, double length, double height, double angle) {
+        super(center, length, height, angle, Material.REFLECTIUM);
+        setMass(0);
+    }
+
+    public WorldBound(Vec2D center, double length, double height) {
+        this(center, length, height, 0);
+    }
+
+    @Override
+    public Vec2D getVelocity() {
+        return Vec2D.ORIGIN;
+    }
+
+    @Override
+    public void translate(Vec2D trans) {
+        // Do Nothing.
+    }
+
+    @Override
+    public void move(double dt) {
+        // Do nothing. Because world bounds are immovable.
+    }
+
+    @Override
+    public void setVelocity(Vec2D velocity) {
+        // Do nothing. World bounds are immovable.
+    }
 }
