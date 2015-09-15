61c8715b3f5630f82dc99e00decf8a313038db5c
afsheen <afsheen_rane@hotmail.com>
2015-09-15 16:50:23 -0400

    Added in friction coefficients for each material.

--- src/phys2d/entities/Material.java
+++ src/phys2d/entities/Material.java
@@ -7,126 +7,170 @@ public enum Material {
     // COR, DENSITY
     /**
      * COR: 0.828 <br>
-     * DENSITY: 1.1
+     * DENSITY: 1.1 <br>
+     * &mu;<sub>S</sub>: 0.9 <br>
+     * &mu;<sub>d</sub>: 0.8
      */
-    RUBBER(0.828, 1.1, Color.LIGHT_GRAY),
+    RUBBER(0.828, 1.1, 0.9, 0.8, Color.LIGHT_GRAY),
 
     /**
      * COR: 1 <br>
      * DENSITY: 1
+     * &mu;<sub>S</sub>: 0 <br>
+     * &mu;<sub>d</sub>: 0
      */
-    REFLECTIUM(1, 1, Color.GREEN),
+    REFLECTIUM(1, 1, 0, 0, Color.GREEN),
 
     /**
      * COR: 0.597 <br>
      * DENSITY: 7.82
+     * &mu;<sub>S</sub>: 0.4 <br>
+     * &mu;<sub>d</sub>: 0.3
      */
-    STEEL(0.597, 7.82, Color.GRAY),
+    STEEL(0.597, 7.82, 0.4, 0.3, Color.GRAY),
 
     /**
      * COR: 0.05 <br>
      * DENSITY: 0.87
+     * &mu;<sub>S</sub>: 0.2 <br>
+     * &mu;<sub>d</sub>: 0.1
      */
-    BUTTER(0.05, 0.87, Color.YELLOW),
+    BUTTER(0.05, 0.87, 0.2, 0.15, Color.YELLOW),
 
     /**
      * COR: 0.25 <br>
      * DENSITY: 2.1
+     * &mu;<sub>S</sub>: 0.75 <br>
+     * &mu;<sub>d</sub>: 0.65
      */
-    DIRT(0.25, 2.1, new Color(95, 50, 0)),
+    DIRT(0.25, 2.1, 0.75, 0.65, new Color(95, 50, 0)),
 
     /**
      * COR: 0.18 <br>
      * DENSITY: 2.1
+     * &mu;<sub>S</sub>: 0.5 <br>
+     * &mu;<sub>d</sub>: 0.45
      */
-    GRASS(0.18, 2.1, new Color(0, 128, 0)),
+    GRASS(0.18, 2.1, 0.5, 0.45, new Color(0, 128, 0)),
 
     /**
      * COR: 0.13 <br>
      * DENSITY: 1.1
+     * &mu;<sub>S</sub>: 0.7 <br>
+     * &mu;<sub>d</sub>: 0.6
      */
-    FLESH(0.13, 1.1, new Color(245, 188, 189)),
+    FLESH(0.13, 1.1, 0.7, 0.6, new Color(245, 188, 189)),
 
     /**
      * COR: 0.6 <br>
      * DENSITY: 0.8
+     * &mu;<sub>S</sub>: 0.4 <br>
+     * &mu;<sub>d</sub>: 0.35
      */
-    WOOD(0.6, 0.8, new Color(165, 85, 0, 255)),
+    WOOD(0.6, 0.8, 0.4, 0.35, new Color(165, 85, 0, 255)),
 
     /**
      * COR: 0.66 <br>
      * DENSITY: 2.5
+     * &mu;<sub>S</sub>: 0.3 <br>
+     * &mu;<sub>d</sub>: 0.2
      */
-    GLASS(0.66, 2.5, new Color(225, 225, 225, 20)),
+    GLASS(0.66, 2.5, 0.3, 0.2, new Color(225, 225, 225, 20)),
 
     /**
      * COR: 0 <br>
      * DENSITY: 100
+     * &mu;<sub>S</sub>: 1 <br>
+     * &mu;<sub>d</sub>: 1
      */
-    INERTIUM(0, 100, new Color(0, 0, 0)),
+    INERTIUM(0, 100, 1, 1, new Color(0, 0, 0)),
 
     /**
      * COR: 0.1 <br>
      * DENSITY: 1
+     * &mu;<sub>S</sub>: 0.9 <br>
+     * &mu;<sub>d</sub>: 0.85
      */
-    REF10(0.1, 1, new Color(0, 30, 10)),
+    REF10(0.1, 1, 0.9, 0.85, new Color(0, 30, 10)),
 
     /**
      * COR: 0.2 <br>
      * DENSITY: 1
+     * &mu;<sub>S</sub>: 0.8 <br>
+     * &mu;<sub>d</sub>: 0.75
      */
-    REF20(0.2, 1, new Color(0, 50, 15)),
+    REF20(0.2, 1, 0.8, 0.75, new Color(0, 50, 15)),
 
     /**
      * COR: 0.3 <br>
      * DENSITY: 1
+     * &mu;<sub>S</sub>: 0.7 <br>
+     * &mu;<sub>d</sub>: 0.65
      */
-    REF30(0.3, 1, new Color(0, 70, 20)),
+    REF30(0.3, 1, 0.7, 0.65, new Color(0, 70, 20)),
 
     /**
      * COR: 0.4 <br>
      * DENSITY: 1
+     * &mu;<sub>S</sub>: 0.6 <br>
+     * &mu;<sub>d</sub>: 0.55
      */
-    REF40(0.4, 1, new Color(0, 90, 25)),
+    REF40(0.4, 1, 0.6, 0.55, new Color(0, 90, 25)),
 
     /**
      * COR: 0.5 <br>
      * DENSITY: 1
+     * &mu;<sub>S</sub>: 0.5 <br>
+     * &mu;<sub>d</sub>: 0.45
      */
-    REF50(0.5, 1, new Color(0, 110, 30)),
+    REF50(0.5, 1, 0.5, 0.45, new Color(0, 110, 30)),
 
     /**
      * COR: 0.6 <br>
      * DENSITY: 1
+     * &mu;<sub>S</sub>: 0.4 <br>
+     * &mu;<sub>d</sub>: 0.35
      */
-    REF60(0.6, 1, new Color(0, 130, 35)),
+    REF60(0.6, 1, 0.4, 0.35, new Color(0, 130, 35)),
 
     /**
      * COR: 0.7 <br>
      * DENSITY: 1
+     * &mu;<sub>S</sub>: 0.3 <br>
+     * &mu;<sub>d</sub>: 0.25
      */
-    REF70(0.7, 1, new Color(0, 150, 40)),
+    REF70(0.7, 1, 0.3, 0.25, new Color(0, 150, 40)),
 
     /**
      * COR: 0.8 <br>
      * DENSITY: 1
+     * &mu;<sub>S</sub>: 0.2 <br>
+     * &mu;<sub>d</sub>: 0.15
      */
-    REF80(0.8, 1, new Color(0, 170, 45)),
+    REF80(0.8, 1, 0.2, 0.15, new Color(0, 170, 45)),
 
     /**
      * COR: 0.9 <br>
      * DENSITY: 1
+     * &mu;<sub>S</sub>: 0.1 <br>
+     * &mu;<sub>d</sub>: 0.05
      */
-    REF90(0.9, 1, new Color(0, 190, 50));
+    REF90(0.9, 1, 0.1, 0.05, new Color(0, 190, 50));
 
     protected final double restitution;
     protected final double density; // 1 kg per 1m^2
 
+    protected double staticFric;
+    protected double dynFric;
+
     protected final Color color;
 
-    private Material(double restitution, double density, Color color) {
+    private Material(double restitution, double density, double statFric,
+            double dynFric, Color color) {
         this.restitution = restitution;
         this.density = density;
+        this.staticFric = statFric;
+        this.dynFric = dynFric;
         this.color = color;
     }
 
@@ -135,6 +179,20 @@ public enum Material {
      */
     public double getRestitution() {
         return restitution;
+    }
+
+    /**
+     * @return the staticFric
+     */
+    public double getStaticFric() {
+        return staticFric;
+    }
+
+    /**
+     * @return the dynFric
+     */
+    public double getDynFric() {
+        return dynFric;
     }
 
     /**
